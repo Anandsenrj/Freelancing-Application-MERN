97d7a408ecf63c060b3512fb8b6ed518abe05b43
Anand Sen <84634909+Anandsenrj@users.noreply.github.com>
2026-04-09 02:11:14 +0530

    Update Navbar links and button labels

--- freelancer-app/client/src/components/Navbar.js
+++ freelancer-app/client/src/components/Navbar.js
@@ -2,15 +2,15 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-<nav className="navbar navbar-expand-lg navbar-dark bg-transparent">
-    <div className="container">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-transparent">
+      <div className="container">
         <Link className="navbar-brand" to="/">SB Works</Link>
 
         <div>
           <Link className="btn btn-outline-light m-1" to="/projects">Projects</Link>
-          <Link className="btn btn-outline-light m-1" to="/new-project">Post Project</Link>
+          <Link className="btn btn-outline-light m-1" to="/new-project">Post</Link>
           <Link className="btn btn-outline-light m-1" to="/freelancer">Dashboard</Link>
-          <Link className="btn btn-outline-light m-1" to="/admin">Admin</Link>
+          <Link className="btn btn-outline-light m-1" to="/chat">Chat</Link>
           <Link className="btn btn-warning m-1" to="/login">Login</Link>
         </div>
       </div>
