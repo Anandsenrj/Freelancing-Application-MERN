8c2935f26f01ad1db5763552a5d7c913607bcff4
Anand Sen <84634909+Anandsenrj@users.noreply.github.com>
2026-04-09 00:50:23 +0530

    Add FreelancerDashboard component

---
+++ freelancer-app/client/src/pages/FreelancerDashboard.js
@@ -0,0 +1,10 @@
+export default function FreelancerDashboard() {
+  return (
+    <div>
+      <h2>Freelancer Dashboard</h2>
+      <p>My Projects</p>
+      <p>Applications</p>
+      <p>Funds</p>
+    </div>
+  );
+}
