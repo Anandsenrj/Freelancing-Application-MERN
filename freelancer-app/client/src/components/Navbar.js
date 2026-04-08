97bc9c2373d79fe882593636112adf45aac58405
Anand Sen <84634909+Anandsenrj@users.noreply.github.com>
2026-04-09 01:21:26 +0530

    Create Navbar.js

---
+++ freelancer-app/client/src/components/Navbar.js
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <Link className="navbar-brand" to="/">SB Works</Link>
+
+        <div>
+          <Link className="btn btn-outline-light m-1" to="/projects">Projects</Link>
+          <Link className="btn btn-outline-light m-1" to="/new-project">Post Project</Link>
+          <Link className="btn btn-outline-light m-1" to="/freelancer">Dashboard</Link>
+          <Link className="btn btn-outline-light m-1" to="/admin">Admin</Link>
+          <Link className="btn btn-warning m-1" to="/login">Login</Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
