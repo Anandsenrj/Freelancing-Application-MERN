5a000c44e85d15fabbfc543cffeeb4ca4d864e1c
Anand Sen <84634909+Anandsenrj@users.noreply.github.com>
2026-04-09 02:15:10 +0530

    Add Chat route and reintroduce dashboards

--- freelancer-app/client/src/App.js
+++ freelancer-app/client/src/App.js
@@ -1,12 +1,12 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Landing from "./pages/Landing";
 import Auth from "./pages/Auth";
-import FreelancerDashboard from "./pages/FreelancerDashboard";
-import AdminDashboard from "./pages/AdminDashboard";
 import AllProjects from "./pages/AllProjects";
 import NewProject from "./pages/NewProject";
+import FreelancerDashboard from "./pages/FreelancerDashboard";
+import AdminDashboard from "./pages/AdminDashboard";
+import Chat from "./pages/Chat";
 
 function App() {
   return (
@@ -14,10 +14,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Auth />} />
-        <Route path="/freelancer" element={<FreelancerDashboard />} />
-        <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/projects" element={<AllProjects />} />
         <Route path="/new-project" element={<NewProject />} />
+        <Route path="/freelancer" element={<FreelancerDashboard />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/chat" element={<Chat />} />
       </Routes>
     </BrowserRouter>
   );
