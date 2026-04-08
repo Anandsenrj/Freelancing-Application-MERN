06dfb9f35eec4c8ab519d419d561c48bb9d7c3b4
Anand Sen <84634909+Anandsenrj@users.noreply.github.com>
2026-04-09 01:12:14 +0530

    Refactor Landing page with Bootstrap styling

--- freelancer-app/client/src/pages/Landing.js
+++ freelancer-app/client/src/pages/Landing.js
@@ -1,15 +1,11 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
 export default function Landing() {
   return (
-    <div>
-      <h1>SB Works</h1>
-      <p>Freelancing Platform</p>
+    <div className="container text-center mt-5">
+      <h1 className="display-4">SB Works</h1>
+      <p className="lead">Freelancing Platform</p>
 
-      <Link to="/login">Login</Link>
-      <br />
-      <Link to="/projects">Browse Projects</Link>
+      <a href="/login" className="btn btn-primary m-2">Login</a>
+      <a href="/projects" className="btn btn-success m-2">Browse Projects</a>
     </div>
   );
 }
