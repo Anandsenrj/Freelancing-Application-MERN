3bf50538e734ffb722ff8d2bc533bc95c3b13e85
Anand Sen <84634909+Anandsenrj@users.noreply.github.com>
2026-04-09 00:53:14 +0530

    Update App.js

--- freelancer-app/client/src/App.js
+++ freelancer-app/client/src/App.js
@@ -1,16 +1,26 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
 
-function App(){
-  return(
+import Landing from "./pages/Landing";
+import Auth from "./pages/Auth";
+import FreelancerDashboard from "./pages/FreelancerDashboard";
+import AdminDashboard from "./pages/AdminDashboard";
+import AllProjects from "./pages/AllProjects";
+import NewProject from "./pages/NewProject";
+
+function App() {
+  return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/login" element={<Login/>}/>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Auth />} />
+        <Route path="/freelancer" element={<FreelancerDashboard />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/projects" element={<AllProjects />} />
+        <Route path="/new-project" element={<NewProject />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
 export default App;
