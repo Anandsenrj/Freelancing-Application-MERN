a2f3c4011cd07020115dbd7f21e7c67b24e91ad1
Anand Sen <84634909+Anandsenrj@users.noreply.github.com>
2026-04-09 01:51:42 +0530

    Update AdminDashboard.js

--- freelancer-app/client/src/pages/AdminDashboard.js
+++ freelancer-app/client/src/pages/AdminDashboard.js
@@ -5,23 +5,24 @@ export default function AdminDashboard() {
     <>
       <Navbar />
       <div className="container mt-4">
-        <h2>Admin Dashboard</h2>
+        <h2 className="mb-4">Admin Dashboard</h2>
 
-       <div className="row text-center">
-  <div className="col-md-6">
-    <div className="card p-4 shadow">
-      <h4>Total Users</h4>
-      <h2>10</h2>
-    </div>
-  </div>
+        <div className="row text-center">
+          <div className="col-md-6">
+            <div className="card p-4 shadow">
+              <h4>Total Users</h4>
+              <h2>10</h2>
+            </div>
+          </div>
 
-  <div className="col-md-6">
-    <div className="card p-4 shadow">
-      <h4>Total Projects</h4>
-      <h2>6</h2>
-    </div>
-  </div>
-</div>      </div>
+          <div className="col-md-6">
+            <div className="card p-4 shadow">
+              <h4>Total Projects</h4>
+              <h2>6</h2>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
