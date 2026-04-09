f2238cd79e56e8894c781443f51c209f18c27a2f
Anand Sen <84634909+Anandsenrj@users.noreply.github.com>
2026-04-09 16:22:07 +0530

    Update Navbar.js

--- freelancer-app/client/src/components/Navbar.js
+++ freelancer-app/client/src/components/Navbar.js
@@ -2,19 +2,16 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-<nav className="navbar navbar-expand-lg navbar-dark bg-dark bg-opacity-75 shadow">
-    <div className="container">
-        <Link className="navbar-brand" to="/">SB Works</Link>
+    <nav className="navbar navbar-light bg-white shadow-sm px-4">
+      <h4 className="text-danger">SB Works</h4>
 
-        <div>
-          <Link className="btn btn-outline-light m-1" to="/projects">Projects</Link>
-          
-          <Link className="btn btn-outline-light m-1" to="/new-project">Post</Link>
-          <Link className="btn btn-outline-light m-1" to="/freelancer">Dashboard</Link>
-          <Link className="btn btn-outline-light m-1" to="/chat">Chat</Link>
-          <Link className="btn btn-warning m-1" to="/login">Sign In</Link>
-          <Link className="btn btn-warning m-1" to="/login">Login</Link>
-        </div>
+      <div>
+        <Link className="mx-2" to="/dashboard">Dashboard</Link>
+        <Link className="mx-2" to="/projects">All Projects</Link>
+        <Link className="mx-2" to="/my-projects">My Projects</Link>
+        <Link className="mx-2" to="/applications">Applications</Link>
+        <Link className="mx-2" to="/chat">Chat</Link>
+        <Link className="mx-2" to="/login">Logout</Link>
       </div>
     </nav>
   );
