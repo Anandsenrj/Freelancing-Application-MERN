3831ff20310f5ea3aa4084385c207a0d3790c7cb
Anand Sen <84634909+Anandsenrj@users.noreply.github.com>
2026-04-09 16:07:42 +0530

    Add 'Post' link to Navbar component

--- freelancer-app/client/src/components/Navbar.js
+++ freelancer-app/client/src/components/Navbar.js
@@ -8,9 +8,11 @@ export default function Navbar() {
 
         <div>
           <Link className="btn btn-outline-light m-1" to="/projects">Projects</Link>
+          
           <Link className="btn btn-outline-light m-1" to="/new-project">Post</Link>
           <Link className="btn btn-outline-light m-1" to="/freelancer">Dashboard</Link>
           <Link className="btn btn-outline-light m-1" to="/chat">Chat</Link>
+          <Link className="btn btn-warning m-1" to="/login">Sign In</Link>
           <Link className="btn btn-warning m-1" to="/login">Login</Link>
         </div>
       </div>
