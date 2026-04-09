073ab801c95161b7268a7c7dd5299431a928a4de
Anand Sen <84634909+Anandsenrj@users.noreply.github.com>
2026-04-09 18:21:49 +0530

    Update projectRoutes.js

--- freelancer-app/server/routes/projectRoutes.js
+++ freelancer-app/server/routes/projectRoutes.js
@@ -3,19 +3,29 @@ import Project from "../models/Project.js";
 
 const router = express.Router();
 
+// GET ALL
 router.get("/", async (req, res) => {
-  res.json(await Project.find());
+  const data = await Project.find();
+  res.json(data);
 });
 
+// CREATE
 router.post("/", async (req, res) => {
   const p = new Project(req.body);
   await p.save();
   res.json(p);
 });
 
+// DELETE
 router.delete("/:id", async (req, res) => {
   await Project.findByIdAndDelete(req.params.id);
-  res.json({ msg: "deleted" });
+  res.json({ message: "Deleted" });
+});
+
+// STATS
+router.get("/stats", async (req, res) => {
+  const total = await Project.countDocuments();
+  res.json({ totalProjects: total });
 });
 
 export default router;
