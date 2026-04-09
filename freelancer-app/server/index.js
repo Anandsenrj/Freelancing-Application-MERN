b610f54b53f77434ff296b72f83fb54da45ffcda
Anand Sen <84634909+Anandsenrj@users.noreply.github.com>
2026-04-09 15:44:46 +0530

    Update index.js

--- freelancer-app/server/index.js
+++ freelancer-app/server/index.js
@@ -1,20 +1,27 @@
-import Project from "./models/Project.js";
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
 
-mongoose.connection.once("open", async () => {
-  const count = await Project.countDocuments();
+import projectRoutes from "./routes/projectRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
-  if (count === 0) {
-    await Project.insertMany([
-      {
-        title: "Logo Design",
-        description: "Design logo for startup",
-        budget: 2000
-      },
-      {
-        title: "Website Development",
-        description: "Build MERN website",
-        budget: 8000
-      }
-    ]);
-  }
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// ROUTES
+app.use("/api/projects", projectRoutes);
+app.use("/api/users", userRoutes);
+
+// CONNECT MONGODB
+mongoose.connect("mongodb://127.0.0.1:27017/freelancer");
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB Connected");
+});
+
+// SERVER
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
 });
