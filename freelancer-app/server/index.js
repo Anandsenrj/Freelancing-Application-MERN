cf0748e681510b6ede3fb7e45aa660a047236de1
Anand Sen <84634909+Anandsenrj@users.noreply.github.com>
2026-04-09 16:05:59 +0530

    Update index.js

--- freelancer-app/server/index.js
+++ freelancer-app/server/index.js
@@ -1,27 +1,29 @@
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
+import Project from "./models/Project.js";
 
-import projectRoutes from "./routes/projectRoutes.js";
-import userRoutes from "./routes/userRoutes.js";
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-// ROUTES
-app.use("/api/projects", projectRoutes);
-app.use("/api/users", userRoutes);
-
-// CONNECT MONGODB
-mongoose.connect("mongodb://127.0.0.1:27017/freelancer");
-
-mongoose.connection.once("open", () => {
+mongoose.connection.once("open", async () => {
   console.log("MongoDB Connected");
-});
 
-// SERVER
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+  const count = await Project.countDocuments();
+
+  if (count === 0) {
+    await Project.insertMany([
+      {
+        title: "Website Development",
+        description: "Build a MERN website for business",
+        budget: 8000
+      },
+      {
+        title: "Mobile App UI",
+        description: "Design UI for Android app",
+        budget: 4000
+      },
+      {
+        title: "Logo Design",
+        description: "Create logo for startup",
+        budget: 2000
+      }
+    ]);
+
+    console.log("Dummy data inserted");
+  }
 });
