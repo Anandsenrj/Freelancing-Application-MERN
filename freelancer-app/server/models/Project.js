8d0315eafdac6abdae6e8373b751455888b52f9f
Anand Sen <84634909+Anandsenrj@users.noreply.github.com>
2026-04-09 16:37:40 +0530

    Update Project.js

--- freelancer-app/server/models/Project.js
+++ freelancer-app/server/models/Project.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
-const schema=new mongoose.Schema({
-  title:String,
-  description:String,
-  budget:Number
+const schema = new mongoose.Schema({
+  title: String,
+  description: String,
+  budget: Number
 });
 
-export default mongoose.model("Project",schema);
+export default mongoose.model("Project", schema);
